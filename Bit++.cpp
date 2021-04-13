2f0942998cfd6c48d21dd4171af334bcb5a9b812
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-13 21:43:40 +0530

    Check s-> ++/--

---
+++ Bit++.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+#define lli long long int
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+int main()
+{
+    fastio;
+    lli n;
+    cin>>n;
+    lli x=0;
+    string s;
+    for(lli i=0; i<n; i++)
+    {
+        cin>>s;
+        if(s=="++X" || s=="X++")
+            x++;
+        else
+            x--;
+    }
+    cout<<x<<endl;
+    return 0;
+}
