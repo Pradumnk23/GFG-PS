f0e244436c5b74b4d50e635c2fbf3d5d92c8438b
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-06-27 21:21:30 +0530

    curr->next=curr->next->next;

---
+++ Remove duplicate element from sorted Linked List.cpp
@@ -0,0 +1,16 @@
+Node *removeDuplicates(Node *head)
+{
+    Node *curr=head;
+    while(curr!=NULL && curr->next!=NULL)
+    {
+        if(curr->data==curr->next->data)
+        {
+            Node *temp=curr->next;
+            curr->next=curr->next->next;
+            delete(temp);
+        }
+        else
+            curr=curr->next;
+    }
+    return head;
+}
