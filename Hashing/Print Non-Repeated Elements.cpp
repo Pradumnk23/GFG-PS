fc4f1d25b9f49fd231396872da15e81f5e746edf
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-07-20 21:01:00 +0530

    return v;

---
+++ Hashing/Print Non-Repeated Elements.cpp
@@ -0,0 +1,20 @@
+class Solution{
+  public:
+    // arr[]: input array
+    // n: size of array
+    //Function to return non-repeated elements in the array.
+    vector<int> printNonRepeated(int arr[],int n)
+    {
+        //Your code here
+        vector<int>v;
+        unordered_map<int,int>mp;
+        for(int i=0; i<n;i ++)
+            mp[arr[i]]++;
+        for(int i=0; i<n; i++)
+        {
+            if(mp[arr[i]]==1)
+                v.push_back(arr[i]);
+        }
+        return v;
+    }
+};
