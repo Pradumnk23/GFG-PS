8e7820e043aab3e556861a4e5f9aa62b079aedb5
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-08-16 20:48:02 +0530

    L-2

---
+++ Stack/Parenthesis Checker.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool match(char a, char b)
+    {
+        return (a=='(' && b==')') || (a=='{' && b=='}') || (a=='[' && b==']');
+    }
+    bool ispar(string x)
+    {
+        stack<char>s;
+        for(int i=0; i<x.length(); i++)
+        {
+            if(x[i]=='(' || x[i]=='{' || x[i]=='[')
+                s.push(x[i]);
+            else
+            {
+                if(s.empty()==true)
+                    return false;
+                else if(match(s.top(),x[i])==false)
+                    return false;
+                else
+                    s.pop();
+            }
+        }
+        return s.empty();
+    }
+
+};
