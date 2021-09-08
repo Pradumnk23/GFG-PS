317f2a05168f0170c7fee6f9f8fb85ea15ac303b
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-09-08 20:45:09 +0530

    Type of subset sum

---
+++ DP/Perfect Sum Problem.cpp
@@ -0,0 +1,31 @@
+class Solution{
+
+	public:
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        int dp[n+1][sum+1];
+        for(int i=0; i<n+1; i++)
+        {
+            for(int j=0; j<sum+1; j++)
+            {
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                    dp[i][j]=1;
+            }
+        }
+        for(int i=1; i<n+1; i++)
+        {
+            for(int j=1; j<sum+1; j++)
+            {
+                if(arr[i-1]<=j)
+                    dp[i][j] = (dp[i-1][j-arr[i-1]] + dp[i-1][j])%1000000007;
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+	}
+	  
+};
