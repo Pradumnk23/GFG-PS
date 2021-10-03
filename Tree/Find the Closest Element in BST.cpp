6e0d2e0d6d5bed0567085438c67c8b71b1976df9
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-10-03 13:10:37 +0530

    MinDiff

---
+++ Tree/Find the Closest Element in BST.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    void maxAbsDiff(Node *root, int K, int &absMin)
+    {
+        if(root)
+        {
+            absMin = min(absMin, abs(K - root->data));
+            
+            if(root->data > K)
+            {
+                maxAbsDiff(root->left, K, absMin);
+            }
+            else
+            {
+                maxAbsDiff(root->right, K, absMin);
+            }
+        }
+    }
+    
+    public:
+    int minDiff(Node *root, int K)
+    {
+        int absMin = INT_MAX;
+    
+        maxAbsDiff(root, K, absMin);
+        
+        return absMin;
+    }
+};
